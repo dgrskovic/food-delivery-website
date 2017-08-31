1c654703e553086d582aa016f02524b4138dfd98
dgrskovic <dariogrskovic@gmail.com>
2017-08-31 19:12:04 +0200

    Initial commit

---
+++ resources/js/script.js
@@ -0,0 +1,99 @@
+$(document).ready(function () {
+   
+    
+    /* Sticky Navigation */
+    $('.js-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '80px'
+    });
+    
+    
+    /* Scroll on button */
+    $('.js--scroll-to-pricing').click(function () {
+        $('html, body').animate({scrollTop: $('.js-pricing').offset().top}, 1000);
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({scrollTop: $('.js-features').offset().top}, 1000);
+    });
+    
+    
+    /* Scroll on nav link(href=# + id= combination) */
+    $(function() {
+      $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            $('html, body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+ 
+    
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        nav.slideToggle(200);
+        
+        var icon = $('.js--nav-icon i');
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+            } else {
+                icon.addClass('ion-navicon-round');
+                icon.removeClass('ion-close-round');
+            }
+    });
+    
+    var map = new GMaps({
+      div: '.map',
+      lat: 38.7437396,
+      lng: -9.04,
+      zoom: 12
+    });
+    
+    map.addMarker({
+      lat: 38.7437396,
+      lng: -9.14,
+      title: 'Lisbon',
+      infoWindow: {
+      content: '<p>Our Lisbon HQ</p>'
+    }
+    });
+    
+    
+});
